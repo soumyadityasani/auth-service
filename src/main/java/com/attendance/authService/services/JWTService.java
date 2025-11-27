62511e919647e9de62acc181ce962193b9d4ad5c
Soumyaditya Mondal <souhttp@gmail.com>
2025-11-27 18:14:07 +0530

    JWT_EXPIRY variable added in env

--- src/main/java/com/attendance/authService/services/JWTService.java
+++ src/main/java/com/attendance/authService/services/JWTService.java
@@ -48,7 +48,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000*60*60))
+                .expiration(new Date(System.currentTimeMillis() + "${JWT_EXPIRY}"))
                 .and()
                 .signWith(getKey())
                 .compact();
