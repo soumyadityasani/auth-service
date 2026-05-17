bc087d88b88704395bb436d3750366b038c9e943
Soumyaditya Mondal <souhttp@gmail.com>
2026-05-17 17:41:15 +0530

    strength decrease

--- src/main/java/com/attendance/authService/config/SecurityConfig.java
+++ src/main/java/com/attendance/authService/config/SecurityConfig.java
@@ -87,6 +87,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(8);
     }
 }
